24313b5b7471b9b61c6fcebfa5e00575ab510a01
Eric Dominguez <EDominguez@tecnasa.com>
2019-12-11 08:57:01 -0500

    Dapper

---
+++ Commons/Dapper_DB.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+
+namespace Commons
+{
+    /// <summary>
+    /// Solo da el "esqueleto", porque Dapper usa especificamente las clases, y no podemos tener todas las clases de todos los programas aqui
+    /// </summary>
+    class Dapper_DB
+    {
+        private SqlConnection dbCon { get; set; }
+
+        public Dapper_DB(string connString)
+        {
+            dbCon = new SqlConnection(connString);
+        }
+
+        public void dbConOpen()
+        {
+            dbCon.Open();
+        }
+
+        public void dbConClose()
+        {
+            dbCon.Close();
+        }
+
+        //para ejecutar un SELECT
+        //public List<Employee> GetCsv(bool cerrarCon = false)
+        //{
+        //    List<Employee> employees = new List<Employee>();
+        //    try
+        //    {
+        //        if (dbCon.State != System.Data.ConnectionState.Open)
+        //            dbCon.Open();
+        //        string sql = "SELECT ... FROM {table} WHERE ...";
+        //        using (dbCon)
+        //        {
+        //            employees = dbCon.Query<Employee>(sql).ToList();
+        //        }
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        FileLogger.WriteToFile($"Error en la DB : {ex.Message}", FileLogger.LogTipos.ERROR, FileLogger.LogLugares.Log4Net, false);
+        //        if (cerrarCon)
+        //        {
+        //            dbCon.Close();
+        //        }
+        //        return employees;
+        //    }
+        //    finally
+        //    {
+        //        if (dbCon.State == System.Data.ConnectionState.Open && cerrarCon)
+        //            dbCon.Close();
+        //    }
+        //    return employees;
+        //}
+    }
+}
--- Commons/FileLogger.cs
+++ Commons/FileLogger.cs
@@ -56,19 +56,19 @@ namespace Commons
             return resultado;
         }
 
-        public static void WriteToFile(string Message, LogTipos tipo = LogTipos.INFO, bool RegistraFecha = true, LogLugares logLugares = LogLugares.Ambos, log4net.ILog log = null)
+        public static void WriteToFile(string Message, LogTipos tipo = LogTipos.ALL, bool RegistraFecha = false, LogLugares logLugares = LogLugares.Log4Net, log4net.ILog log = null)
         {
             if (log == null)
                 log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
 
+            string path = AppDomain.CurrentDomain.BaseDirectory + "\\Logs";
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
             if (logLugares == LogLugares.Archivo || logLugares == LogLugares.Ambos)
             {
-                string path = AppDomain.CurrentDomain.BaseDirectory + "\\Logs";
-                if (!Directory.Exists(path))
-                {
-                    Directory.CreateDirectory(path);
-                }
-                string Message1 = RegistraFecha ? DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss -> ") + Message : Message;
+                string Message1 = RegistraFecha ? Message : DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss -> ") + Message;
 
                 string fileName = tipo == LogTipos.INFO ? "ServiceLog_" : "ServiceLog_Error_";
                 string filepath = AppDomain.CurrentDomain.BaseDirectory + "\\Logs\\" + fileName + DateTime.Now.Date.ToShortDateString().Replace('/', '_') + ".txt";
@@ -90,13 +90,24 @@ namespace Commons
             }
             if ((logLugares == LogLugares.Log4Net || logLugares == LogLugares.Ambos) && (log != null))
             {
-                if (tipo == LogTipos.INFO)
+                if (tipo == LogTipos.INFO || tipo == LogTipos.ALL)
                     log.Info(Message);
+                else if (tipo == LogTipos.DEBUG)
+                    log.Debug(Message);
+                else if (tipo == LogTipos.WARN)
+                    log.Warn(Message);
                 else if (tipo == LogTipos.ERROR)
                     log.Error(Message);
                 else if (tipo == LogTipos.FATAL)
                     log.Fatal(Message);
             }
+        }
+
+        public static void WriteToFile(string Message, log4net.ILog log)
+        {
+            if (log == null)
+                log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+            WriteToFile(Message, LogTipos.ALL, false, LogLugares.Log4Net, log);
         }
     }
 }
